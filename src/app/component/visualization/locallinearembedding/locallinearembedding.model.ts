0979ced2ed91729e99771f4ee2b38d34d615a1a6
Grettygoose <gheinric@fredhutch.org>
2017-10-13 16:12:40 -0700

    Local Linear Embedding

--- src/app/component/visualization/locallinearembedding/locallinearembedding.model.ts
+++ src/app/component/visualization/locallinearembedding/locallinearembedding.model.ts
@@ -5,20 +5,52 @@ import { DataFieldFactory } from './../../../model/data-field.model';
 import { GraphConfig } from './../../../model/graph-config.model';
 import { DataField } from 'app/model/data-field.model';
 
-export class LocalLinearEmbeddingConfigModel extends GraphConfig {
-    components = 3;
-    dimension = DimensionEnum.THREE_D;
+export class LocalLinearEmbeddingEigenSolver {
+    public static AUTO = 'auto';
+    public static DENSE = 'dense';
+    public static ARPACK = 'arpack';
+}
+
+export class LocalLinearEmbeddingMethod {
+    public static STANDARD = 'standard';
+    public static MODIFIED = 'modified';
+    public static LTSA = 'ltsa';
+    public static HESSIAN = 'hessian';
+}
+
+export class LocalLinearEmbeddingNeighborsAlgorithm {
+    public static AUTO = 'auto';
+    public static BRUTE = 'brute';
+    public static KD_TREE = 'kd_tree';
+    public static BALL_TREE = 'ball_tree';
 }
 
 
+export class LocalLinearEmbeddingConfigModel extends GraphConfig {
+    constructor() {
+        super();
+        this.entity = EntityTypeEnum.SAMPLE;
+        this.visualization = VisualizationEnum.LOCALLY_LINEAR_EMBEDDING;
+    }
+
+    components = 2;
+    dimension = DimensionEnum.THREE_D;
+    n_neighbors = 5;
+    eigen_solver = LocalLinearEmbeddingEigenSolver.AUTO;
+    reg = 0.001;
+    random_state = 'None';
+    neighbors_algorithm = LocalLinearEmbeddingNeighborsAlgorithm.AUTO;
+    LocalLinearEmbeddingMethod = LocalLinearEmbeddingMethod.STANDARD;
+}
 
 
 export interface LocalLinearEmbeddingDataModel extends GraphData {
-    result: Array<Array<number>>;
+    result: any;
     resultScaled: Array<Array<number>>;
     pointColor: Array<number>;
     pointSize: Array<number>;
     pointShape: Array<ShapeEnum>;
     sampleIds: Array<string>;
     markerIds: Array<string>;
+    patientIds: Array<string>;
 }
