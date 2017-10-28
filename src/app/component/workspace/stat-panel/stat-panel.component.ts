a78aa6b292fa1997358d7376eb6008673c9eb7e4
Michael Zager <michael@zager.co>
2017-10-28 05:36:27 -0700

    ...

--- src/app/component/workspace/stat-panel/stat-panel.component.ts
+++ src/app/component/workspace/stat-panel/stat-panel.component.ts
@@ -1,4 +1,4 @@
-import { explainedVariance, explainedVarianceRatio } from './stats-compute';
+import { explainedVariance, explainedVarianceRatio, genericHistogram } from './stats-compute';
 import { StatsFactory } from './stats-factory';
 import { GraphData } from './../../../model/graph-data.model';
 import { INSERT_ANNOTATION } from './../../../action/graph.action';
@@ -21,41 +21,57 @@ declare var vega: any;
 })
 export class StatPanelComponent implements AfterViewInit {
 
+  // @Input() means that you can set the properties in the html reprsentation of this object (eg <app-worspace-stat-pane configA=''>)
   @Input() configA: GraphConfig;
   @Input() configB: GraphConfig;
+  @Input() graphBData: GraphData;
   @Input() set graphAData(value: GraphData){
+
+    // Exit if this is trigger before there is data... Could likely be done better with Rxjs
+    if (value === null) {
+      return;
+    }
+
+    // Save Data Passed In In Local Variable
     this.data = value;
 
+    // Create Array of Possible Stat Types
     this.metrics = [
-      { label: 'Explained Variance', value: explainedVariance(value.result.explainedVariance )},
-      { label: 'Explained Variance Ratio', value: explainedVarianceRatio(value.result.explainedVarianceRatio )},
+      { label: 'Histogram', value: genericHistogram( [] )},
+      // { label: 'Explained Variance', value: explainedVariance( value.result.explainedVariance )},
+      // { label: 'Explained Variance Ratio', value: explainedVarianceRatio( value.result.explainedVarianceRatio )},
     ];
 
+    // Set Metric Creates The Vega Visualization and +'s it to The Page 
    this.setMetric(this.metrics[0]);
 
   }
-  @Input() graphBData: GraphData;
 
+  // @ ViewChild means that you can access the html element in the corresponding html template file
   @ViewChild('chartContainer', { read: ViewContainerRef }) chartContainer: ViewContainerRef;
   @ViewChild('tabs') private tabs: ElementRef;
 
+
+  // Just some properties
   statOptions = ['Graph A', 'Graph B'];
   metrics = [];
   statsFactory: StatsFactory;
   data = {};
 
+  // Create The Vega + Render It
   setMetric( value: any): void {
     new vega.View(vega.parse(value.value), {
       renderer: 'canvas'
       }).initialize('#stat-panel-chart').run();
-
   }
 
 
+  // Constructor Called Automatically 
   constructor(private componentFactoryResolver: ComponentFactoryResolver) {
     this.statsFactory = StatsFactory.getInstance();
   }
 
+  // Ng After View Init get's called after the dom has been constructed
   ngAfterViewInit() {
     $(this.tabs.nativeElement).tabs();
   }
