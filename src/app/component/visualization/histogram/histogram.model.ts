5f3692ff89ff19540fef76e14e9be0dabfb060b2
Michael Zager <michael@zager.co>
2017-11-02 09:08:36 -0700

    ...

--- src/app/component/visualization/histogram/histogram.model.ts
+++ src/app/component/visualization/histogram/histogram.model.ts
@@ -1,22 +1,17 @@
+import { GraphData } from './../../../model/graph-data.model';
 import { Legend } from './../../../model/legend.model';
 import { GraphConfig } from 'app/model/graph-config.model';
 import { VisualizationEnum, DimensionEnum, GraphEnum, EntityTypeEnum } from 'app/model/enum.model';
 import { DataField, DataFieldFactory } from 'app/model/data-field.model';
 
 export class HistogramConfigModel extends GraphConfig {
-    
+
     displayType: DimensionEnum = DimensionEnum.THREE_D;
     domain: Array<number> = [-500, 500];
     showAllGenes: Boolean = false;
     showCytobands: Boolean = true;
 }
 
-export interface HistogramDataModel {
-    legends: Array<Legend>;
-    genes: any;
-    bands: any;
-    chromo: Array<{'chr': string, 'P': number, 'C': number, 'Q': number}>;
-    showAllGenes: Boolean;
-    showBands: Boolean;
-    allowRotation: Boolean;
+export interface HistogramDataModel extends GraphData {
+    x: number;
 }
