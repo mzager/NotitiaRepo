421d305dfc8d1a17b7f66c75027413c2052c7209
grettygoose <gheinric@fredhutch.org>
2017-11-30 17:12:02 -0800

    vega

--- src/app/model/stat.model.ts
+++ src/app/model/stat.model.ts
@@ -1,6 +1,22 @@
 import { GraphData } from './graph-data.model';
 import { VisualizationEnum, StatTypeEnum, ChartTypeEnum } from 'app/model/enum.model';
 
+/* GENERAL note: Visualization = is the graph A/B, example PCA Fast ICA, PCA. Stat = result data coming from ski-kit
+Visalization, Graph = is what is final rendering
+*/
+
+/*
+This is not a IS A (extends) or a HAS A (contains), this is an interface, ACTS-LIKE "contract" polymorphic
+
+Each stat needs to have a class of either a SINGLE value, a 1D array or a 2D array. This helps to orgainize the GraphData results
+coming back from Visualization and to control the chartType it will export.
+
+type = readonly because we never want anyone/anything to change that, single will always be single etc
+name = name of graph
+charts = come from the ChartTypeEnum, PIE = 1, DONUT = 2, etc.
+data: under stat data can be anything, as the classes get seperated data becomes more specific
+*/
+
 export interface Stat {
     name: string;
     type: StatTypeEnum;
@@ -20,7 +36,7 @@ export class StatSingle implements Stat {
 }
 
 export class StatOneD implements Stat {
-    readonly type =  StatTypeEnum.ONE_D;
+    readonly type = StatTypeEnum.ONE_D;
     charts: Array<ChartTypeEnum> = [ChartTypeEnum.PIE, ChartTypeEnum.DONUT, ChartTypeEnum.HISTOGRAM];
     name: string;
     data: Array<{ label: string, value: number, color?: number }>;
@@ -42,39 +58,204 @@ export class StatTwoD implements Stat {
     }
 }
 
-
 // Factory Pattern
 export class VegaFactory {
 
-        // Singleton Pattern
-        private static _instance: VegaFactory = null;
-        public static getInstance(): VegaFactory {
-            if (VegaFactory._instance === null) { VegaFactory._instance = new VegaFactory(); }
-            return VegaFactory._instance;
-        }
-        private constructor() { }
-
-
-        // Public Interface + Takes The Visualzion Type and figures which to call
-        public getVegaObject(stat: Stat, chartType: ChartTypeEnum): any {
-            return (chartType === ChartTypeEnum.DONUT) ? this.createDonut(stat) :
-                (chartType === ChartTypeEnum.HISTOGRAM) ? this.createHistogram(stat) :
-                null;
-        }
-
-        private createDonut(stat: Stat): any {
-            return null;
-        }
-
-        private createHistogram(stat: Stat): any {
-            return null;
-        }
+    // Singleton Pattern- copied from a previous factory - get more info but structure so you can only have 1 Vega Factory
+    private static _instance: VegaFactory = null;
+    public static getInstance(): VegaFactory {
+        if (VegaFactory._instance === null) { VegaFactory._instance = new VegaFactory(); }
+        return VegaFactory._instance;
     }
+    private constructor() { }
+
+
+    /*
+    Public Interface + Takes The graph Type and figures which to call, EXAMPLE: if DONUT create donut with stat variable,
+    that requires a name, type, chart and data
+    */
+    public getVegaObject(stat: Stat, chartType: ChartTypeEnum): any {
+        return (chartType === ChartTypeEnum.DONUT) ? this.createDonut(stat) :
+            (chartType === ChartTypeEnum.HISTOGRAM) ? this.createHistogram(stat) :
+                (chartType === ChartTypeEnum.PIE) ? this.createPie(stat) :
+                    (chartType === ChartTypeEnum.LINE) ? this.createLine(stat) :
+                        (chartType === ChartTypeEnum.LABEL) ? this.createLabel(stat) :
+                            (chartType === ChartTypeEnum.SCATTER) ? this.createScatter(stat) :
+                                null;
+    }
+
+    // humm, what is passed here, anything that works for a chartType.DONUT? = StatOneD, maybe selected from a dropdown??
+    private createDonut(stat: Stat): any {
+        const values = stat.data;
+        const vega = {
+            '$schema': 'https://vega.github.io/schema/vega/v3.0.json',
+            'width': 250,
+            'height': 250,
+            'padding': 25,
+            'autosize': { 'type': 'fit', 'resize': true },
+            'data': [
+                {
+                    'name': 'table',
+                    'values': values,
+                    'transform': [
+                        {
+                            'type': 'pie',
+                            'field': 'value',
+                        }
+                    ]
+                }
+            ],
+            'scales': [
+                {
+                    'name': 'r',
+                    'type': 'sqrt',
+                    'domain': {
+                        'data': 'table',
+                        'field': 'value'
+                    }
+                },
+                {
+                    'name': 'color',
+                    'type': 'ordinal',
+                    'domain': {
+                        'data': 'table',
+                        'field': 'label'
+                    },
+                    'range': {
+                        'scheme': 'spectral'
+                    }
+                }
+            ],
+            'marks': [
+                {
+                    'type': 'arc',
+                    'from': {
+                        'data': 'table'
+                    },
+                    'encode': {
+                        'enter': {
+                            'x': {
+                                'field': {
+                                    'group': 'width'
+                                },
+                                'mult': 0.5
+                            },
+                            'y': {
+                                'field': {
+                                    'group': 'height'
+                                },
+                                'mult': 0.5
+                            },
+                            'startAngle': {
+                                'field': 'startAngle'
+                            },
+                            'endAngle': {
+                                'field': 'endAngle'
+                            },
+                            'padAngle': {
+                                'value': 0.035
+                            },
+                            'innerRadius': {
+                                'value': 60
+                            },
+                            'outerRadius': {
+                                'signal': 'width / 2'
+                            },
+                            'cornerRadius': {
+                                'value': 0
+                            },
+                            // How its done in Vega land, uncommented part works but color hovers do not
+                            // ,tooltip': {'signal': 'datum['field']+ '%''}
+                            'tooltip': { 'signal': 'datum.value' }
+                        },
+                        // opacity change on hover
+                        'update': {
+                            'fill': {
+                                'scale': 'color',
+                                'field': 'value'
+                            },
+                            'fillOpacity': {
+                                'value': 1
+                            }
+                        },
+                        'hover': {
+                            'fillOpacity': {
+                                'value': 0.5
+                            },
+                            'text': {
+                                'field': 'value'
+                            }
+                        }
+                    }
+                },
+                {
+                    'type': 'text',
+                    'from': {
+                        'data': 'table'
+                    },
+                    'encode': {
+                        'enter': {
+                            'x': {
+                                'field': {
+                                    'group': 'width'
+                                },
+                                'mult': 0.5
+                            },
+                            'y': {
+                                'field': {
+                                    'group': 'height'
+                                },
+                                'mult': 0.5
+                            },
+                            'radius': {
+                                'scale': 'r',
+                                'field': 'value',
+                                'offset': 90
+                            },
+                            'theta': {
+                                'signal': '(datum.startAngle + datum.endAngle)/2'
+                            },
+                            'align': {
+                                'value': 'center'
+                            },
+                            'text': {
+                                'field': 'label'
+                            },
+                            'font': {
+                                'value': 'Lato'
+                            },
+                            'fontSize': {
+                                'value': 14
+                            },
+                            'tooltip': { 'signal': 'datum.value' }
+                        }
+                    }
+                }
+            ]
+        };
+        return vega;
+    }
+    private createHistogram(stat: Stat): any {
+        return null;
+    }
+    private createPie(stat: Stat): any {
+        return null;
+    }
+    private createLine(stat: Stat): any {
+        return null;
+    }
+    private createLabel(stat: Stat): any {
+        return null;
+    }
+    private createScatter(stat: Stat): any {
+        return null;
+    }
+}
 
 // Factory Pattern
 export class StatFactory {
 
-    // Singleton Pattern
+    // Singleton Pattern- copied logic
     private static _instance: StatFactory = null;
     public static getInstance(): StatFactory {
         if (StatFactory._instance === null) { StatFactory._instance = new StatFactory(); }
@@ -85,14 +266,19 @@ export class StatFactory {
     // Public Interface + Takes The Visualzion Type and figures which to call
     public getStatObjects(data: GraphData, vis: VisualizationEnum): Array<Stat> {
 
-        return (vis === VisualizationEnum.INCREMENTAL_PCA) ? this.chartIncrementalPca(data, vis) :
-               (vis === VisualizationEnum.FAST_ICA) ? this.createFastIca(data, vis) :
-        null;
+        return (vis === VisualizationEnum.INCREMENTAL_PCA) ? this.chartIncrementalPca(data) :
+            (vis === VisualizationEnum.FAST_ICA) ? this.createFastIca(data, vis) :
+                (vis === VisualizationEnum.MDS) ? this.createMds(data, vis) :
+                    (vis === VisualizationEnum.TRUNCATED_SVD) ? this.createTruncatedSvd(data, vis) :
+                        (vis === VisualizationEnum.KERNAL_PCA) ? this.createKernalPca(data, vis) :
+                            (vis === VisualizationEnum.SPARSE_PCA) ? this.createSparse_PCA(data, vis) :
+                                (vis === VisualizationEnum.DICTIONARY_LEARNING) ? this.createDictionaryLearning(data, vis) :
+                                    null;
     }
 
-    private chartIncrementalPca(data: GraphData, visualization: VisualizationEnum): Array<Stat> {
+    private chartIncrementalPca(data: GraphData): Array<Stat> {
 
-        // single values
+        // stats array
         const stats = [
             new StatTwoD('components', data.result.components),
             new StatOneD('Explained Variance', this.formatPrincipleComponents(data.result.explainedVariance)),
@@ -112,9 +298,27 @@ export class StatFactory {
     private createFastIca(data: any, visualization: VisualizationEnum): Array<Stat> {
         return null;
     }
-    
+    private createMds(data: any, visualization: VisualizationEnum): Array<Stat> {
+        return null;
+    }
+    private createTruncatedSvd(data: any, visualization: VisualizationEnum): Array<Stat> {
+        return null;
+    }
+    private createSparse_PCA(data: any, visualization: VisualizationEnum): Array<Stat> {
+        return null;
+    }
+    private createKernalPca(data: any, visualization: VisualizationEnum): Array<Stat> {
+        return null;
+    }
+    private createDictionaryLearning(data: any, visualization: VisualizationEnum): Array<Stat> {
+        return null;
+    }
+
     formatPrincipleComponents(data: Array<number>): Array<{ label: string, value: number, color?: number }> {
-        return data.map( (v, i) => ({label: 'PC' + (i + 1), value: v }) );
+        return data.map((v, i) => ({ label: 'PC' + (i + 1), value: v }));
+    }
+    formatMarkerComponents(data: Array<number>): Array<{ label: string, value: number, color?: number }> {
+        return null;
     }
 
 
