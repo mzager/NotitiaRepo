e10a33a319f29d40618d3de9e2636679821bc860
Michael Zager <michael@zager.co>
2017-12-04 14:15:52 -0800

    ...

--- src/app/component/workspace/edge-panel/edge-panel.component.ts
+++ src/app/component/workspace/edge-panel/edge-panel.component.ts
@@ -3,7 +3,6 @@ import { DataField } from 'app/model/data-field.model';
 import { INSERT_ANNOTATION } from './../../../action/graph.action';
 import { StatsInterface } from './../../../model/stats.interface';
 import { FormBuilder } from '@angular/forms';
-import { VegaFactory } from './../../../service/vega.factory';
 import { GraphConfig } from './../../../model/graph-config.model';
 import { GraphData } from './../../../model/graph-data.model';
 import { Component, ComponentFactoryResolver, Input, Output, ViewContainerRef,
