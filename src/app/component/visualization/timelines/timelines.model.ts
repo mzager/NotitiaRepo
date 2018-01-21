075067984bfa62c315922c52a1318a369b43fc40
Michael Zager <michael@zager.co>
2018-01-20 16:52:19 -0800

    ...

--- src/app/component/visualization/timelines/timelines.model.ts
+++ src/app/component/visualization/timelines/timelines.model.ts
@@ -6,7 +6,7 @@ import { DataField, DataFieldFactory } from 'app/model/data-field.model';
 export class TimelinesStyle {
     public static NONE = 'None';
     public static CONTINUOUS = 'Continuous Bar';
-    public static TICKS = 'Ticks';
+    public static TICKS = 'Bars';
     public static ARCS = 'Arcs';
     public static SYMBOLS = 'Symbols';
 }
@@ -22,8 +22,8 @@ export class TimelinesConfigModel extends GraphConfig {
     sort = 'Death';
     attrs = ['age at diagnosis'];
     bars = [
-        { 'label': 'Treatment', 'style': 'Ticks', 'events': ['Drug', 'Radiation'] },
-        { 'label': 'Status', 'style': 'Ticks', 'events': ['Follow Up', 'Diagnosis','Death'] }
+        { 'label': 'Status', 'style': 'Bars', 'events': ['Follow Up', 'Diagnosis', 'Death'] },
+        { 'label': 'Treatment', 'style': 'Bars', 'events': ['Drug', 'Radiation'] }
     ];
 }
 
