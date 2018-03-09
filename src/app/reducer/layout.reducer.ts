84d9edf0dd81601b0f2bafd5d9c7eab789c4dd87
Michael Zager <michael@zager.co>
2018-03-09 09:50:12 -0800

    ...

--- src/app/reducer/layout.reducer.ts
+++ src/app/reducer/layout.reducer.ts
@@ -18,7 +18,7 @@ export interface State {
 const initialState: State = {
     graphPanelA: e.GraphPanelEnum.GRAPH_A,
     graphPanelB: e.GraphPanelEnum.NONE,
-    modalPanel: e.PanelEnum.LANDING,
+    modalPanel: e.PanelEnum.DATA,
     loader: false,
     workspaceConfig: new WorkspaceConfigModel()
 };
