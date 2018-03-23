44c8537089e7dde98413f1a395e837b59edf1979
Michael Zager <michael@zager.co>
2018-03-23 08:54:51 -0700

    ...

--- src/app/component/workspace/loader/loader.component.ts
+++ src/app/component/workspace/loader/loader.component.ts
@@ -14,9 +14,10 @@ import {
 -->
     <div class='loader-copy-container'>
         <div class='loader-copy'>
-            &#39;{{quote.q}}&#39;
-            {{quote.a}}
+            <div class='loader-quote'>&#39;{{quote.q}}&#39;</div>
+            <div class='loader-author'>{{quote.a}}</div>
         </div>
+        <div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
     </div>
 
 </div>`,
@@ -41,8 +42,9 @@ export class LoaderComponent implements AfterViewInit, OnDestroy {
             { q: 'Nothing cannot exist forever.', a: 'Stephen Hawking' },
             { q: 'I think computer viruses should count as life.', a: 'Stephen Hawking' },
             { q: 'There could be shadow galaxies, shadow stars, and even shadow people.', a: 'Stephen Hawking' },
-            { q: 'I was not a good student. I did not spend much time at college; I was too busy enjoying myself.', a: 'Stephen Hawking' },
-            { q: 'If you torture the data long enough, it will confess to anything', a: '' }
+            { q: 'I was not a good student. I was too busy enjoying myself.', a: 'Stephen Hawking' },
+            { q: 'If you torture the data long enough, it will confess t o anything', a: 'Ronald Coase' },
+            { q: 'Nothing cannot exist forever.', a: 'Stephen Hawking' }
         ];
         this.quote = this.quotes[Math.floor(Math.random() * this.quotes.length) + 1];
     }
