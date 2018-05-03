8b14e97ae7e6e76d1c8bb7996a5310ca25f21ccd
Michael Zager <michael@zager.co>
2018-05-03 11:09:06 -0700

    ...

--- src/app/component/workspace/citations-panel/citations-panel.component.ts
+++ src/app/component/workspace/citations-panel/citations-panel.component.ts
@@ -1,3 +1,4 @@
+import { ViewEncapsulation } from '@angular/compiler/src/core';
 import { Observable } from 'rxjs/Observable';
 import { ModalService } from 'app/service/modal-service';
 import { FormBuilder, FormGroup } from '@angular/forms';
@@ -18,37 +19,9 @@ declare var $: any;
 @Component({
     selector: 'app-workspace-citations-panel',
     styleUrls: ['./citations-panel.component.scss'],
-    template:
-        `
-<div class="citations-modal-panel">
-    <div class='row'>
-        <a href='#' class='modalClose' (click)='closeClick()'></a>
-        <div class='col s12 m9'>
-            <h1 class='citations-h1'>Site Citations</h1>
-            <div class="input-field citations-search col s12">
-            <i class="material-icons prefix">search</i>
-            <input id="icon_prefix" type="text" #filterInput class="validate">
-          </div>
-            <div *ngFor='let result of results'>
-                <span *ngIf='result.visible'>
-                    <div *ngFor='let method of result.methods'>
-                        <h2 class='citations-h2'>{{method.method}}</h2>
-                        <p class='citations-main-link'>{{method.url}}</p>
-                    </div>
-                    <div *ngFor='let citation of result.citations'>
-                        <p class='citations-name'>{{citation.name}}</p>
-                        <p class='citations-desc'> {{citation.desc}}</p>
-                        <p class='citations-link'><a href='{{citation.url}}' target='_blank'>{{citation.url}}</a></p>
-                        <p class='citations-link-2'><a href='{{citation.url}}' target='_blank'>{{citation.url2}}</a></p>
-                        <p class='citations-link-2'><a href='{{citation.url}}' target='_blank'>{{citation.url3}}</a></p>
-                    </div>
-                </span>
-            </div>
-        </div>
-    </div>
-</div>
-`,
-    changeDetection: ChangeDetectionStrategy.OnPush
+    templateUrl: './citations-panel.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    encapsulation: ViewEncapsulation.None
 })
 export class CitationsPanelComponent implements AfterViewInit, OnInit, OnDestroy {
 
