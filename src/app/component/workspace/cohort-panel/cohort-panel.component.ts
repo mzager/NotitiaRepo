260bc8c2edcc6fdffce65ad9513f67faf8446b07
Michael Zager <michael@zager.co>
2018-05-03 11:36:02 -0700

    ...

--- src/app/component/workspace/cohort-panel/cohort-panel.component.ts
+++ src/app/component/workspace/cohort-panel/cohort-panel.component.ts
@@ -6,7 +6,7 @@ import { EntityTypeEnum } from './../../../model/enum.model';
 import { DataField } from 'app/model/data-field.model';
 import {
   Component, Input, Output, EventEmitter, AfterViewInit,
-  OnInit, ViewChild, ElementRef, ChangeDetectionStrategy, ChangeDetectorRef
+  OnInit, ViewChild, ElementRef, ChangeDetectionStrategy, ChangeDetectorRef, ViewEncapsulation
 } from '@angular/core';
 import { VisualizationEnum, DirtyEnum } from 'app/model/enum.model';
 import { Legend } from 'app/model/legend.model';
@@ -15,67 +15,9 @@ declare var $: any;
 @Component({
   selector: 'app-workspace-cohort-panel',
   styleUrls: ['./cohort-panel.component.scss'],
-  template: `
-  <div class='cohort-modal-panel'>
-    <div class='row'>
-        <a href='#' class='modalClose' (click)='closeClick()'></a>
-        <div class="col s12">
-        <h1 class='cohort-h1'>Cohorts</h1>
-        <h2 class='cohort-h2'>Create, Manage and Apply custom cohorts to your visualizations
-            <a href='https://www.youtube.com/embed/XQu8TTBmGhA' target='_blank'>
-                <i class='small material-icons modalWatchVideoIcon'>ondemand_video</i>Watch Tutorial</a>
-        </h2>
-        </div>
-    </div>
-    <div class='row'>
-        <!-- My Cohorts -->
-        <div class='col s12 l3 cohort-my-list'>
-            <span>My Cohorts</span>
-            <div *ngFor='let myCohort of cohorts'>
-                <div class='cohort-myRow' (click)='deleteClick(myCohort)'>
-                    <i class='material-icons cohort-myRow-delete'>remove_circle_outline</i>
-                    <span>{{myCohort.n}}</span>
-                </div>
-            </div>
-        </div>
-        <div class='col s12 l9 cohort-container'>
-            <span class='cohort-span'>Build A Cohort</span>
-            <div class='col s12 cohort-field '>
-                <label for='cohortName'>Create</label>
-                <input id='cohortName' type='text' placeholder='Enter Cohort Name' [(ngModel)]='activeCohort.n'>
-            </div>
-            <div class='col s12 cohort-field' [class.cohort-field-or]='condition.condition==="or"'
-            *ngFor='let condition of activeCohort.conditions'>
-                <label>{{condition.condition}}</label>
-                <select class='cohort-field-dropdown browser-default' [(ngModel)]='condition.field' materialize='material_select'>
-                    <option *ngFor='let option of fields' [ngValue]='option'>{{option.name}}</option>
-                </select>
-                <select *ngIf='condition.field.type==="category"' class='cohort-field-dropdown browser-default'
-                [(ngModel)]='condition.value'
-                    materialize='material_select'>
-                    <option *ngFor='let option of condition.field.options' [ngValue]='option.value'>{{option.name}}</option>
-                </select>
-                <span  *ngIf='condition.field.type==="number"' class='cohort-minMax'>
-                    <input type='number' [(ngModel)]='condition.min' placeholder='Min'
-                    style='margin-bottom:5px;border-color:#EEE;width:65px;padding-left: 6px;'>
-                    <input type='number' [(ngModel)]='condition.max' placeholder='Max'
-                    style='margin-bottom:5px;border-color:#EEE;width:65px;padding-left: 6px;'>
-                </span>
-                <div class='cohort-field-buttons'>
-                    <a href='#' (click)='fieldAnd(condition)'>And</a> |
-                    <a href='#' (click)='fieldOr(condition)'>Or</a>
-                    <span *ngIf='condition.condition !== "where"'> |
-                        <a href='#' (click)='fieldDel(condition)'>Remove</a>
-                    </span>
-                </div>
-            </div>
-            <div class=' col s12 cohort-button'>
-                <a class='cohort-save' href='#' (click)='saveClick()'>Save</a>
-            </div>
-        </div>
-    </div>
-</div>`,
-  changeDetection: ChangeDetectionStrategy.Default
+  templateUrl: './cohort-panel.component.html',
+  changeDetection: ChangeDetectionStrategy.Default,
+  encapsulation: ViewEncapsulation.None
 })
 export class CohortPanelComponent implements AfterViewInit {
 
