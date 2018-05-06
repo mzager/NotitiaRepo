b9a40b7a419567544b328a04fad21397e8f45580
Michael Zager <michael@zager.co>
2018-05-06 08:25:25 -0700

    ...

--- src/app/component/visualization/pcaincremental/pcaincremental.form.component.ts
+++ src/app/component/visualization/pcaincremental/pcaincremental.form.component.ts
@@ -4,62 +4,15 @@ import { DimensionEnum, EntityTypeEnum, CollectionTypeEnum } from './../../../mo
 import { GraphConfig } from './../../../model/graph-config.model';
 import { DataTypeEnum, DirtyEnum } from 'app/model/enum.model';
 import { DataField, DataFieldFactory, DataTable } from './../../../model/data-field.model';
-import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy, ViewEncapsulation } from '@angular/core';
 import { FormControl, FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 import * as _ from 'lodash';
 
 @Component({
   selector: 'app-pcaincremental-form',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  template: `
-<form [formGroup]='form' novalidate>
-<!--
-  <mat-form-field class='form-field'>
-    <mat-select placeholder='Data' formControlName='table'>
-      <mat-option *ngFor='let option of dataOptions' [value]='option.label'>
-          {{ option.label }}
-      </mat-option>
-    </mat-select>
-  </mat-form-field>
-  <mat-form-field class='form-field'>
-    <mat-select placeholder='Visualize' formControlName='entity'>
-      <mat-option *ngFor='let option of displayOptions' [value]='option'>
-          {{ option }}
-      </mat-option>
-    </mat-select>
-  </mat-form-field>
-  -->
-  <mat-form-field class='form-field'>
-    <mat-select placeholder='X Axis' formControlName='pcx'>
-        <mat-option *ngFor='let option of PcOptions' [value]='option'>
-            PC {{ option }}
-        </mat-option>
-    </mat-select>
-  </mat-form-field>
-  <mat-form-field class='form-field'>
-    <mat-select placeholder='Y Axis' formControlName='pcy'>
-        <mat-option *ngFor='let option of PcOptions' [value]='option'>
-            PC {{ option }}
-        </mat-option>
-    </mat-select>
-  </mat-form-field>
-  <mat-form-field class='form-field'>
-    <mat-select placeholder='Z Axis' formControlName='pcz'>
-        <mat-option *ngFor='let option of PcOptions' [value]='option'>
-            PC {{ option }}
-        </mat-option>
-    </mat-select>
-  </mat-form-field>
-   <div class='form-group'>
-    <div class='switch'>
-      <label><span class='form-label'>Whiten</span>
-        <input type='checkbox' formControlName='whiten'>
-        <span class='lever'></span>
-      </label>
-    </div>
-  </div>
-</form>
-`
+  templateUrl: './pcaincremental.form.component.html',
+  encapsulation: ViewEncapsulation.None
 })
 export class PcaIncrementalFormComponent extends AbstractScatterForm {
 
