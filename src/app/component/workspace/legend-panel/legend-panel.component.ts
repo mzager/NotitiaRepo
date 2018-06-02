fe87c139f7f8efbe16e0f738eb1529c878d2efef
Michael Zager <michael@zager.co>
2018-06-02 12:18:29 -0700

    ...

--- src/app/component/workspace/legend-panel/legend-panel.component.ts
+++ src/app/component/workspace/legend-panel/legend-panel.component.ts
@@ -1,3 +1,6 @@
+import { Observable } from 'rxjs/Rx';
+import { Subscription } from 'rxjs/Subscription';
+import { Subject } from 'rxjs/Subject';
 import { DataDecorator, DataDecoratorTypeEnum } from './../../../model/data-map.model';
 import { Legend } from './../../../model/legend.model';
 import { GraphData } from './../../../model/graph-data.model';
@@ -6,7 +9,7 @@ import { interpolateSpectral } from 'd3-scale-chromatic';
 
 import {
   Component, Input, Output, ChangeDetectionStrategy, EventEmitter, AfterViewInit,
-  ViewChild, ElementRef, ChangeDetectorRef, ViewEncapsulation
+  ViewChild, ElementRef, ChangeDetectorRef, ViewEncapsulation, OnDestroy
 } from '@angular/core';
 import { ShapeEnum, SizeEnum, DataTypeEnum } from 'app/model/enum.model';
 import * as d3Interpolate from 'd3-interpolate';
@@ -15,7 +18,8 @@ import * as d3Shape from 'd3-shape';
 import * as d3 from 'd3';
 import { GraphConfig } from '../../../model/graph-config.model';
 import { DomSanitizer } from '@angular/platform-browser';
-declare var $: any;
+import * as _ from 'lodash';
+
 
 @Component({
   selector: 'app-workspace-legend-panel',
@@ -24,60 +28,45 @@ declare var $: any;
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None
 })
-export class LegendPanelComponent implements AfterViewInit {
+export class LegendPanelComponent implements AfterViewInit, OnDestroy {
 
   public allLegends: Array<Legend> = [];
-  // public items: Array<{ label: string, value: string }>;
+  public updateLegend = _.debounce(this.update, 600);
 
   private _config: GraphConfig;
   @Input() public set config(value: GraphConfig) {
-    console.log(value);
-    // if (value === undefined) { return; }
-    // if (value === null) { return; }
-    // this._config = value;
-    // this.updateLegend();
+    if (value === null) { return; }
+    this._config = value;
+    this.updateLegend();
   }
 
-  private _decorators: Array<Legend> = [];
+  private _decorators: Array<DataDecorator> = [];
   @Input() public set decorators(value: Array<DataDecorator>) {
-    if (value === undefined) { return; }
     if (value === null) { return; }
-    this._decorators = value.map(decorator => {
-      if (decorator.legend !== null) {
-        this.formatValues(decorator.legend);
-      }
-      return decorator.legend;
-    });
+    this._decorators = value;
     this.updateLegend();
   }
 
   public _legends: Array<Legend> = [];
   @Input() public set legends(value: Array<Legend>) {
-    if (value === undefined) { return; }
     if (value === null) { return; }
-    if (value.length === 0) { return; }
-    value.forEach(legend => {
-      this.formatValues(legend);
-    });
     this._legends = value;
     this.updateLegend();
   }
 
-  formatValues(legend: Legend): void {
-    if (legend.type === 'COLOR') {
-      for (let i = 0; i < legend.values.length; i++) {
-        if (!isNaN(legend.values[i])) {
-          legend.values[i] = '#' + (0xffffff + legend.values[i] + 1).toString(16).substr(1);
-        }
-      }
-    }
-    // else if (legend.type === 'SHAPE') {
-    //   debugger;
-    //   for (let i = 0; i < legend.values.length; i++) {
-    //     legend.values[i] = './assets/shapes/shape-' + legend.values[i] + '-solid-legend.png';
-    //   }
-    // }
-  }
+  //   // formatValues(legend: Legend): void {
+  //   if(legend.type === 'COLOR') {
+  //   for (let i = 0; i < legend.values.length; i++) {
+  //     if (!isNaN(legend.values[i])) {
+  //       legend.values[i] = '#' + (0xffffff + legend.values[i] + 1).toString(16).substr(1);
+  //     }
+  //   }
+  // } else if (legend.type === 'SHAPE') {
+  //   for (let i = 0; i < legend.values.length; i++) {
+  //     legend.values[i] = './assets/shapes/shape-' + legend.values[i] + '-solid-legend.png';
+  //   }
+  // }
+  // }
   public select(): void {
   }
 
@@ -85,17 +74,35 @@ export class LegendPanelComponent implements AfterViewInit {
   }
 
   ngAfterViewInit(): void {
+  }
+
+  ngOnDestroy() {
 
   }
 
-  public updateLegend(): void {
-    // if (!this._config) { return; }
-    // console.dir(this._config);
-    this.allLegends = [].concat(...this._decorators, ...this._legends);
-    // requestAnimationFrame(v => {
-    console.log("!!!");
-    //   this.cd.markForCheck();
-    // });
+  legendFormatter(legend: Legend): Legend {
+    const rv = Object.assign({}, legend);
+    if (rv.type === 'COLOR') {
+      for (let i = 0; i < rv.values.length; i++) {
+        if (!isNaN(rv.values[i])) {
+          legend.values[i] = '#' + (0xffffff + legend.values[i] + 1).toString(16).substr(1);
+        }
+      }
+    } else if (legend.type === 'SHAPE') {
+      for (let i = 0; i < rv.values.length; i++) {
+        if (!isNaN(rv.values[i])) {
+          legend.values[i] = './assets/shapes/shape-' + legend.values[i] + '-solid-legend.png';
+        }
+      }
+    }
+    return rv;
+  }
+
+  public update(): void {
+    const decorators = this._decorators.map(decorator => this.legendFormatter(decorator.legend));
+    const legends = this._legends.map(legend => this.legendFormatter(legend));
+    this.allLegends = [].concat(...decorators, ...legends);
+    this.cd.detectChanges();
   }
 
   constructor(public cd: ChangeDetectorRef) { }
