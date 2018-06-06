8d180688706245cb49e08eaeeabb1f23e875c10b
Michael Zager <michael@zager.co>
2018-06-06 09:08:24 -0700

    ...

--- runBuild.sh
+++ runBuild.sh
@@ -1,1 +1,3 @@
+npm run compute
+npm run loader
 ng build --prod --optimization=true --outputHashing="all" --sourceMap=false --extractCss=true --namedChunks=false --aot=true --extractLicenses=true --vendorChunk=false --buildOptimizer=true
