3dbc92dd6ccc1726ed03a9414ab644a1c16e96a9
Michael Zager <michael@zager.co>
2018-06-09 11:28:43 -0700

    ...

--- src/app/component/workspace/tool-bar/tool-bar.component.ts
+++ src/app/component/workspace/tool-bar/tool-bar.component.ts
@@ -1,11 +1,10 @@
-import { WorkspaceConfigModel } from 'app/model/workspace.model';
 import {
-  ChangeDetectionStrategy, OnInit, EventEmitter, Input, Output,
-  Component, ChangeDetectorRef, ViewEncapsulation
+  ChangeDetectionStrategy, ChangeDetectorRef,
+  Component, EventEmitter, Input, OnInit, Output,
+  ViewEncapsulation
 } from '@angular/core';
-import { GraphEnum, GraphActionEnum, ToolEnum, WorkspaceLayoutEnum } from 'app/model/enum.model';
-import { ElementRef } from '@angular/core/src/linker/element_ref';
-import { ViewChild } from '@angular/core/src/metadata/di';
+import { WorkspaceLayoutEnum } from 'app/model/enum.model';
+import { WorkspaceConfigModel } from 'app/model/workspace.model';
 // import * as dat from 'dat.gui';
 declare var dat: any;
 
@@ -19,11 +18,7 @@ declare var dat: any;
 export class ToolBarComponent implements OnInit {
 
   hideButtons = false;
-  // @Input() selectedTool: ToolEnum;
-  // @Input() selectedGraph: GraphEnum;
-  // @Output() selectTool = new EventEmitter();
-  // @Output() selectGraph = new EventEmitter();
-  // @Output() graphAction = new EventEmitter();
+
   @Input() set workspaceConfig(value: WorkspaceConfigModel) {
     this.hideButtons = (value.layout === WorkspaceLayoutEnum.SINGLE);
     this.cd.detectChanges();
