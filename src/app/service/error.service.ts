64c5e3ecceaee90b06ed1d2cf6c9552e1b7ebd35
Michael Zager <michael@zager.co>
2018-06-09 09:51:06 -0700

    ...

---
+++ src/app/service/error.service.ts
@@ -0,0 +1,19 @@
+import { ErrorHandler, Injectable } from '@angular/core';
+import * as StackTrace from 'stacktrace-js';
+
+@Injectable()
+export class ErrorService implements ErrorHandler {
+    handleError(error): void {
+        StackTrace.fromError(error).then(stackframes => {
+            const stackString = stackframes
+                .splice(0, 20)
+                .map(function (sf) {
+                    return sf.toString();
+                }).join('\n');
+
+            console.log(error + '\n\n' + stackString, 'background: #000; color: red;');
+            // alert(error + '\n:' + stackString);
+        });
+    }
+    contructor() { }
+}
