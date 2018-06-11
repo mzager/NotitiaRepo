ad707500f2051cf419d0c8955ea5a2972ee7eb20
grettygoose <gheinric@fredhutch.org>
2018-06-11 09:04:49 -0700

    machine learning

--- src/app/component/visualization/truncatedsvd/truncatedsvd.model.ts
+++ src/app/component/visualization/truncatedsvd/truncatedsvd.model.ts
@@ -2,9 +2,9 @@ import { DimensionEnum, EntityTypeEnum, VisualizationEnum } from 'app/model/enum
 import { GraphConfig } from './../../../model/graph-config.model';
 import { GraphData } from './../../../model/graph-data.model';
 
-export class TruncatedSvdAlgorithem {
+export class TruncatedSvdAlgorithm {
     public static RANDOMIZED = 'randomized';
-    public static ARPACK = 'arpack';
+    // public static ARPACK = 'arpack';
 }
 
 export class TruncatedSvdConfigModel extends GraphConfig {
@@ -15,7 +15,7 @@ export class TruncatedSvdConfigModel extends GraphConfig {
         this.label = 'Truncated SVD';
     }
     dimension = DimensionEnum.THREE_D;
-    algorithm = TruncatedSvdAlgorithem.RANDOMIZED;
+    algorithm = TruncatedSvdAlgorithm.RANDOMIZED;
     n_components = 10;
     tol = 0;
     n_iter = 5;
