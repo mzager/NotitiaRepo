3d705b481c680059adf0b8b4b0c2767f925a168a
Zager <michael@zager.co>
2018-09-06 12:59:35 -0700

    ...

--- src/app/component/visualization/umap/umap.compute.ts
+++ src/app/component/visualization/umap/umap.compute.ts
@@ -17,15 +17,21 @@ export const umapCompute = (
         data: matrix.data,
         n_neighbors: config.n_neighbors,
         min_dist: config.min_dist,
-        metric: config.metric
+        metric: config.metric,
+        n_components: config.n_components,
+        spread: config.spread,
+        set_op_mix_ratio: config.set_op_mix_ratio,
+        target_weight: config.target_weight,
+        target_n_neighbors: config.target_n_neighbors,
+        angular_rp_forest: config.angular_rp_forest,
+        transform_queue_size: config.transform_queue_size,
+        repulsion_strength: config.repulsion_strength,
+        local_connectivity: config.local_connectivity,
+        negative_sample_rate: config.negative_sample_rate,
+        learning_rate: config.learning_rate
       })
       .then(result => {
-        // result.resultScaled = worker.util.scale3d(
-        //   result.result,
-        //   config.pcx - 1,
-        //   config.pcy - 1,
-        //   config.pcz - 1
-        // );
+        result.resultScaled = worker.util.scale3d(result.result);
         result.sid = matrix.sid;
         result.mid = matrix.mid;
         result.pid = matrix.pid;
