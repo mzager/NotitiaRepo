05227ffd0781a4285b9d52aa5abbc9d7857e8bab
grettygoose <gheinric@fredhutch.org>
2019-01-30 14:23:22 -0800

    new dashboard

--- src/app/component/workspace/dashboard-panel/dashboard-panel.component.ts
+++ src/app/component/workspace/dashboard-panel/dashboard-panel.component.ts
@@ -57,6 +57,7 @@ export class DashboardPanelComponent implements AfterViewInit, OnDestroy {
   private statFactory: StatFactory;
   private container: any;
 
+
   @ViewChild('chartContainer') chartContainer: ElementRef;
   @Output() hide = new EventEmitter<any>();
   private _config: GraphConfig;
@@ -76,57 +77,60 @@ export class DashboardPanelComponent implements AfterViewInit, OnDestroy {
       const allResults = results.reduce((p, c) => p.concat(...c), []);
 
       allResults.forEach(result => {
-        // data
-        const data = result.stats[1].data as Array<{ mylabel: string; myvalue: number; color?: any }>;
-        const dataName = result.stats[1].name;
-        console.log(data);
 
-        const w = 400;
-        const h = 300;
+        // grab cohort name NEED break at cohort loop
+        // tslint:disable-next-line:max-line-length
+        // this.chartContainer.nativeElement.apply('<div style="font-size:1.2rem; font-weight: 300; margin-bottom:50px; margin-top:50px; text-transform:uppercase; letter-spacing: 1px; color: #1e88e5;">'
+        // + result.cohort.n + '</div>');
+            // tslint:disable-next-line:max-line-length
+        //     this.chartContainer.nativeElement.innerHTML += '<div style="font-size:1.2rem; font-weight: 300; margin-bottom:50px; margin-top:50px; text-transform:uppercase; letter-spacing: 1px; color: #1e88e5;">'
+        // + result.cohort.n + '</div>';
+        // d3
+        // .select(this.chartContainer.nativeElement)
+        // .append('<div>hi</div>')
+const c =  this.chartContainer.nativeElement as any;
+c.insertAdjacentHTML('beforeend', '<div style="font-size:1.2rem; font-weight: 300; margin-bottom:50px; margin-top:50px; text-transform:uppercase; letter-spacing: 1px; color: #1e88e5;">'+result.cohort.n+'</div>' );
+
+        debugger;
+
+        result.stats.forEach(stat => {
+
+        const myData = stat.data as Array<{ mylabel: string; myvalue: number; color?: any }>;
+        const myTitle = stat.name;
+
+        const w = 500;
+        const h = 400;
         // margins
-        const margin = { top: 30, bottom: 70, left: 30, right: 10 };
+        const margin = { top: 40, bottom: 70, left: 30, right: 10 };
 
         // width & height
         const width = w - margin.left - margin.right;
         const height = h - margin.top - margin.bottom;
 
-        // colors
+                // colors
         const color = [
-        '#b3e5fc',
-        '#81d4fa',
-        '#4fc3f7',
+        '#e3f2fd',
+        '#bbdefb',
+        '#90caf9',
         '#29b6f6',
-        '#03a9f4',
-        '#039be5',
-        '#f44336',
-        '#e91e63',
-        '#9c27b0',
-        '#673ab7',
-        '#3f51b5',
+        '#64b5f6',
+        '#42a5f5',
         '#2196f3',
-        '#03a9f4',
-        '#00bcd4',
-        '#009688',
-        '#4caf50',
-        '#8bc34a',
-        '#cddc39',
-        '#ffeb3b',
-        '#ffc107',
-        '#ff9800',
-        '#ff5722',
-        '#795548',
-        '#9e9e9e',
-        '#607d8b' ];
+        '#1e88e5',
+        '#1976d2',
+        '#1565c0',
+        '#0d47a1',
+                    ];
 
         // Scales
         const xScale = d3.scaleBand()
           .domain(
-            data.map(d => (d.mylabel)))
+            myData.map(d => (d.mylabel)))
           .range([margin.left, width])
           .padding(0.1);
 
         const yScale = d3.scaleLinear()
-          .domain([0, d3.max(data, (d => (d.myvalue)))])
+          .domain([0, d3.max(myData, (d => (d.myvalue)))])
           .range([height, margin.top]);
 
         // Call yAxis & assign tick number
@@ -149,7 +153,7 @@ export class DashboardPanelComponent implements AfterViewInit, OnDestroy {
         svg
         // draw bars
           .selectAll('rect')
-          .data(data)
+          .data(myData)
           .enter()
           .append('rect')
           .attr('class', 'bar')
@@ -169,7 +173,7 @@ export class DashboardPanelComponent implements AfterViewInit, OnDestroy {
         svg
           // bar value labels
           .selectAll('.val-label')
-          .data(data)
+          .data(myData)
           .enter()
           .append('text')
           .attr('x', (d => (xScale(d.mylabel) + xScale.bandwidth() / 2)))
@@ -187,7 +191,7 @@ export class DashboardPanelComponent implements AfterViewInit, OnDestroy {
         svg
           // x-axis labels
           .selectAll('.bar-label')
-          .data(data)
+          .data(myData)
           .enter()
           .append('text')
           .attr('class', 'xAxisLabels')
@@ -206,31 +210,10 @@ export class DashboardPanelComponent implements AfterViewInit, OnDestroy {
         .attr('y', 10)
         .attr('text-anchor', 'middle')
         .attr('class', 'title')
-        .text(dataName);
-
-
-        // // TODO : Need to figure out what's wrong with the data for year of death.
-        // result.stats = result.stats.filter(v => v.name !== 'year of death');
-        // // const id = 'cc' + Math.random().toString(36).substring(7);
-        // // tslint:disable-next-line:max-line-length
-        // const cohortDiv = this.container.append(
-        //   '<div style='font-size:2rem; font-weight: 300; margin-bottom:20px; margin-top:10px;'>' +
-        //     result.cohort.n +
-        //     '</div>'
-        // );
-        // result.stats.forEach(stat => {
-        //   const id2 =
-        //     'cc' +
-        //     Math.random()
-        //       .toString(36)
-        //       .substring(7);
-        //   const div = cohortDiv.append(
-        //     '<div id='' + id2 + '' style='display:inline-block;padding-bottom:40px;padding-right:20px;'></div>'
-        //   );
-        //   this.statVegaFactory.drawChartObject(stat, ChartTypeEnum.HISTOGRAM, id2, div);
-        // });
+        .text(myTitle);
       });
-    });
+    }, this);
+  });
   }
 
   closeClick(): void {
@@ -240,7 +223,7 @@ export class DashboardPanelComponent implements AfterViewInit, OnDestroy {
   ngOnDestroy(): void {}
   ngAfterViewInit(): void {
     this.statFactory = StatFactory.getInstance(this.dataService);
-    // this.statVegaFactory = StatVegaFactory.getInstance();
+
     this.drawStats();
   }
   constructor(private cd: ChangeDetectorRef, private dataService: DataService) {}
